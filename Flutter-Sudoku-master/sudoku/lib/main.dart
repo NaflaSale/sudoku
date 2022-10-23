cbccb70353b74c63bd18375368bec8f83dcaef31
Nafla Alanazi <92294615+NaflaSale@users.noreply.github.com>
2022-10-23 14:17:03 +0300

    Update main.dart
    update

--- Flutter-Sudoku-master/sudoku/lib/main.dart
+++ Flutter-Sudoku-master/sudoku/lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Styles.primaryColor,
       ),
-      home: const SplashScreenPage(),
+      home: const HomePage(),
     );
   }
 }
